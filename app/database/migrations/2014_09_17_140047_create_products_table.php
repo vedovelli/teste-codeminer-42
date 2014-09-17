29314a9adc85ada7f92c201e69d9645c61f22ce8
Vedovelli <vedovelli@gmail.com>
2014-09-17 11:11:00 -0300

    Migration for products

---
+++ app/database/migrations/2014_09_17_140047_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('products', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('lm');
+			$table->string('name');
+			$table->boolean('free_shipping')->default(0);
+			$table->string('description');
+			$table->float('price');
+			$table->string('category');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('products');
+	}
+
+}
