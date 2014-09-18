ed1aa442774d936415d442a8a03fa317a2d46dd5
Vedovelli <vedovelli@gmail.com>
2014-09-18 07:43:11 -0300

    Criada classe responsável por importar os dados enviados no Excel. As instruções para importação foram removidas do controller e adicionados nesta classe

--- app/controllers/FileController.php
+++ app/controllers/FileController.php
@@ -32,31 +32,8 @@ class FileController extends \BaseController {
 
 				$file->move($path, $name); // Salva o arquivo em disco
 
-				// Adiciona a tarefa de leitura do arquivo e criação dos registros no banco de dados à fila
-				Queue::push(function($job) use ($path, $name)	{
-
-					Product::truncate(); // Limpa a tabela, evitando assim itens duplicados
-
-					$reader = Excel::selectSheetsByIndex(0)->load($path.'/'.$name); // Le o arquivo Excel
-
-					$reader->each(function($sheet) { // Faz o loop nas linhas que contém conteúdo
-
-						$product = array();
-						$product['lm'] = $sheet->lm;
-						$product['name'] = $sheet->name;
-						$product['free_shipping'] = $sheet->free_shipping;
-						$product['description'] = $sheet->description;
-						$product['price'] = number_format((float) $sheet->price, 2, ',', '.');
-						$product['category'] = $sheet->category;
-
-						// Cria um novo produto baseado nas informações obtidas no arquivo Excel
-						Product::create($product);
-
-					});
-
-					$job->delete(); // Remove job from Queue
-
-				});
+				$product_import = new ProductImport; // Instancia a classe que contem o método responsável por processar o arquivo
+				$product_import = $product_import->doIt($path, $name); // Faz a importação
 
 				// Redireciona para a tela anterior com mensagem de sucesso!
 				return Redirect::to('file')->with('message-success', 'Arquivo enviado com sucesso! <small>[ <a href="/products">ver produtos</a> ]</small>');
